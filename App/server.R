bba5ad7ce4cb61fbd178d8e6e7e8166475516a83
Dan Mazur <danpmazur@gmail.com>
2015-10-07 14:31:04 -0700

    Updated the Shiny App

--- App/server.R
+++ App/server.R
@@ -14,26 +14,46 @@ shinyServer(function(input, output) {
   #  1) It is "reactive" and therefore should re-execute automatically
   #     when inputs change
   #  2) Its output type is a plot
+  partyColours <- setNames(c('#00A7EC','#263893',
+                    '#3D9B35','#D71921','#F78320', 'grey'),
+                    c('New Democratic', 'Conservative', 'Liberal',
+                      'Green', 'Bloc Quebecois', 'Other'))
+  #partyColours <- c('orange', 'blue', 'red', 'green', 'lightblue', 'grey')
+  #names(partyColours) <- c('New Democratic', 'Conservative', 'Liberal',
+  #                         'Green', 'Bloc Quebecois', 'Other')
   
   output$distPlot <- renderPlot({
     mySwaps <- swaps[swaps$Riding1 == input$inRiding1 &
                        swaps$Party1 == input$inParty1, ]
     mySwaps <- mySwaps[with(mySwaps, order(-totalBenefit)), ]
     mySwaps <- subset(mySwaps, ! Party2 %in% input$inVetoParties)
-    names <- gsub(" ", "\n", gsub(" –", "", mySwaps[1:10, ]$Riding2))
-    partyColours <- c('#F78320','#263893','#D71921',
-      '#3D9B35','#00A7EC', 'grey')
-    names(partyColours) <- c('New Democratic', 'Conservative', 'Liberal',
-      'Green', 'Bloc Quebecois', 'Other')
-    colors <- sapply(mySwaps[1:10, "Party2"], function(x){partyColours[x]})
-    op <- par(mar=c(11,4,4,2))
-    barplot
-    barplot(height = as.numeric(mySwaps[1:10,"Benefit1"]),
+    if (nrow(mySwaps) < 10)
+    {
+      plot.new()
+      plot(5,
+           5,
+           type="n",
+           axes=F,
+           ann=T,
+           xlab = "",
+           ylab = "",
+           xlim=c(0,10),
+           ylim=c(0,10),
+           main="No recommendations found. Please select a new riding or party...")
+    }
+    else
+    {
+      names <- gsub(" ", "\n", gsub(" –", "", mySwaps[1:10, ]$Riding2))
+      colors <- as.vector(sapply(mySwaps[1:10, ]$Party2, function(x){partyColours[x]}))
+      op <- par(mar=c(11,4,4,2))
+      barplot
+      barplot(height = as.numeric(mySwaps[1:10,"Benefit1"]),
             names.arg = names, 
             horiz=F,
             las=2,
-            ylab = "Your vote value factor",
+            ylab = "Your benefit factor",
             col=colors)
-    rm(op)
+      rm(op)
+    }
   })
 })
--- App/ui.R
+++ App/ui.R
@@ -24,12 +24,28 @@ shinyUI(fluidPage(
     
     # Show a plot of the generated distribution
     mainPanel(
-      p("This app recommends strategic vote swaps based on current election forecasts."),
-      p("\n"),
-      p("Select your riding and your preferred party."),
-      p("\n"),
-      p("The app will recommend the best ridings to look for swap partners."),
-      plotOutput("distPlot")
+      tabsetPanel(
+        tabPanel("Recommendations",
+          p("This app recommends strategic vote swaps based on current election forecasts."),
+          p("\n"),
+          p("Select your riding and your preferred party."),
+          p("\n"),
+          p("The app will recommend the best ridings to look for swap partners."),
+          plotOutput("distPlot")
+        ),
+        tabPanel("What is this?",
+                 p("The recommendations provided by this site are based on the strategic value of Canadians' votes."),
+                 p("The ten most strategically important swaps for your preferred party in your riding are shown in decending order of importance from left to right. The height of the bars in the plot represent how many times more valuable your vote would be in terms of contributing to a seat in parliament for your preferred party if you vote swapped. Each bar has a colour indicating the party that your vote swap partner supports (i.e. the party that you would physically vote for if you arranged the swap)."),
+                 p("This analysis assumes that the value of a vote is inversely proportional to the number of votes separating your preferred party and the leader in your riding, or their closest competition if they are the leader, according to current elections forecasts."),
+                 p("The election forecasts are provided by ",
+                    a("threehundredeight.com", href="http://www.threehundredeight.com")),
+                 p("More details of this analysis can be found on ", 
+                   a("my blog.", 
+                     href="https://rwuncertainty.wordpress.com/2015/10/04/canadas-voter-inequality-and-the-vote-swapping-economy/")),
+                 p("Analysis details and source code are available on ",
+                   a("my Github. ", href="http://www.github.com/QEDan"), "Pull requests are welcome.")
+                 )
+      )
     )
   )
 ))
